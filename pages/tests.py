1f7f96e0869641eea43c4d70b247337f73e882f9
Matthew Henkel <nerdotron5000@gmail.com>
2023-03-10 14:06:29 -0500

    added dumb tests

---
+++ pages/tests.py
@@ -0,0 +1,17 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+# Create your tests here.
+class HomePageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
